81507980a348ed5d36ee63a3de90d21698e822c5
M4ri4Edd <luzdudinha@gmail.com>
2024-07-18 21:24:26 -0300

    .

--- src/app/componentes/searchBar.js
+++
@@ -1,8 +0,0 @@
-const SearchBar = ({ onSearch }) => (
-    <div className="search-bar">
-      <input type="text" placeholder="Buscar receitas..." onChange={e => onSearch(e.target.value)} />
-    </div>
-  );
-  
-  export default SearchBar;
-  
